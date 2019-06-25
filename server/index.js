e46d229219c157d560c228bcebf569325e8a9e68
Chizindu David <davidchizindu@gmail.com>
2019-06-25 11:53:12 +0100

    Setup backend development environment

---
+++ server/index.js
@@ -0,0 +1,67 @@
+/* eslint-disable no-unused-vars */
+import express from 'express';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import cors from 'cors';
+import errorhandler from 'errorhandler';
+
+dotenv.config();
+const isProduction = process.env.NODE_ENV === 'production';
+
+// Create global app object
+const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+if (!isProduction) {
+  app.use(errorhandler());
+}
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (!isProduction) {
+  app.use((err, req, res, next) => {
+    console.log(err.stack);
+
+    res.status(err.status || 500);
+
+    res.json({
+      errors: {
+        message: err.message,
+        error: err
+      }
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    errors: {
+      message: err.message,
+      error: {}
+    }
+  });
+});
+
+// finally, let's start our server...
+const server = app.listen(process.env.PORT || 3000, () => {
+  console.log(`Listening on port ${server.address().port}`);
+});
+
+export default app;
